7d3a80c7ec01ace37bbf635a83c9dacf539c3d45
HaoWan <wanhao.maple@gmail.com>
2015-07-22 12:07:24 +0300

    first commit

---
+++ js/react/Header.js
@@ -0,0 +1,107 @@
+/** @jsx React.DOM */
+
+'use strict';
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div className="container">
+            <div className="navbar-header">
+                <a className="navbar-brand" href="index.html">中南屋 - 中国在肯负责任投资线上倡导平台</a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="about.html">新闻</a>
+                    </li>
+                    <li>
+                        <a href="services.html">专栏</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">项目</a>
+                    </li>
+                    <li>
+                        <a href="about.html">关于</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">联系方式</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+      </nav>
+    );
+  }
+});
+
+React.render(
+  <Header />,
+  //document.body
+  document.getElementById('header')
+);
+
+/*
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+// tutorial3.js
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+
+// tutorial4.js
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+
+// tutorial5.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+*/
---
+++ js/react/Slide.js
@@ -0,0 +1,55 @@
+
+
+
+var Slide = React.createClass({
+  render: function() {
+    var divStyle = {backgroundImage: 'url(http://placehold.it/1900x1080&text=Slide One)'};
+    return (
+        <header id="myCarousel" className="carousel slide">
+    
+            <ol className="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <div className="carousel-inner">
+                <div className="item active">
+                    <div className="fill" style={{backgroundImage: 'url(http://placehold.it/1900x1080&text=Slide One)'}}></div>
+                    <div className="carousel-caption">
+                        <h2>海报一</h2>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="fill" style={divStyle}></div>
+                    <div className="carousel-caption">
+                        <h2>海报二</h2>
+                    </div>
+                </div>
+                <div className="item">
+                    <div className="fill" style={divStyle}></div>
+                    <div className="carousel-caption">
+                        <h2>海报三</h2>
+                    </div>
+                </div>
+            </div>
+
+
+            <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span className="icon-prev"></span>
+            </a>
+            <a className="right carousel-control" href="#myCarousel" data-slide="next">
+                <span className="icon-next"></span>
+            </a>
+        </header>
+    );
+  }
+});
+// React.render(
+//   <Slide />,
+//   //document.body
+//   document.getElementById('slide')
+// );
+
+
+module.exports = Slide;
